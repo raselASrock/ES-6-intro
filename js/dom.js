aad142a77df47bc9da2a6a26844cb3f03aaa4579
raselASrock <mdraselm413@gmail.com>
2022-08-23 21:17:34 +0600

    revision-recap

--- js/dom.js
+++ js/dom.js
@@ -8,3 +8,14 @@ document.getElementById('center-third').addEventListener('click', function(){
     const thirdFriend = document.getElementById('third-friend');
     thirdFriend.style.textAlign = 'center';
 })
+
+document.getElementById('add-friend').addEventListener('click', function(){
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML = `
+    <h3 class="friend-name">New Friend</h3>
+    <p>Something New Addeded.</p>
+    `
+    friendContainer.appendChild(friend);
+})
